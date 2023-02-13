fb2238955659daa7cad49057f5cc81f4af16fb25
rgrabar <renco.grabar@gmail.com>
2023-02-13 21:55:46 +0100

    smaller epsilon

--- RayTracerChallenge/Helper.h
+++ RayTracerChallenge/Helper.h
@@ -1,5 +1,5 @@
 #pragma once
 #include <cmath>
-const double EPSILON = 0.00001;
+const double EPSILON = 0.00000000001;
 
 inline bool epsilonEqual(double a, double b) { return std::abs(a - b) < EPSILON; };
