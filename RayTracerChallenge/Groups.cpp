5cf921b717636ae6c551bcc58093174dfaa59044
rgrabar <renco.grabar@gmail.com>
2023-03-18 13:58:50 +0100

    Group cpp

--- RayTracerChallenge/Groups.cpp
+++ RayTracerChallenge/Groups.cpp
@@ -1,1 +1,120 @@
 #include "Groups.h"
+
+Group::~Group() {
+	for (auto child : children) {
+		if (dynamic_cast<Group*>(child)) {
+			// cast only to avoid warning
+			delete (Group*)child;
+		}
+	}
+}
+
+//TODO: passing a local group variable as child
+// breaks everything yay
+void Group::addChild(Shape* s) {
+	children.emplace_back(s);
+	s->parent = this;
+
+	// TODO: update bounds if needed
+	boundsOf(true);
+}
+
+// TODO: do this better
+void Group::partitionChildren(Group& leftG, Group& rightG) {
+	BoundingBox left, right;
+
+	auto bounds = m_bounds;
+	bounds.splitBounds(&left, &right);
+
+	std::vector<Shape*> newChildren;
+
+	for (auto child : children) {
+		auto childBounds = child->parentSpaceBoundsOf();
+		if (left.contains(childBounds)) {
+			leftG.addChild(child);
+		}
+		else if (right.contains(childBounds)) {
+			rightG.addChild(child);
+		}
+		else
+			newChildren.emplace_back(child);
+	}
+	children = newChildren;
+}
+
+void Group::makeSubgroup(Group& sub) {
+	auto g = new Group();
+	for (auto s : sub.children) {
+		g->addChild(s);
+	}
+	addChild(g);
+}
+
+
+Intersections Group::intersect(const Ray& ray)const {
+
+	if (!m_bounds.intersect(ray)) {
+		return {};
+	}
+
+	Intersections i;
+
+	for (auto shape : children) {
+		// TODO: do this somehow else, overload intersect to take a vector?
+		auto v = shape->shapeIntersect(ray);
+		for (auto objectIntersect : v.intersections)
+			i.intersections.insert(objectIntersect);
+	}
+	return i;
+}
+
+Tuple Group::objectNormal(const Tuple& objectPoint, const Intersection* hit = nullptr)const {
+	return Tuple::vector(0, 0, 0);
+}
+
+BoundingBox Group::boundsOf(bool update = false) {
+	// TODO: cache the box, don't update if not needed
+	if (update) {
+		auto box = BoundingBox();
+
+		for (auto shape : children) {
+			auto cbox = shape->parentSpaceBoundsOf();
+			box.mergeBox(cbox);
+		}
+
+		m_bounds = box;
+	}
+	return m_bounds;
+}
+
+
+bool Group::includes(const Shape* s)const {
+	return std::find(children.begin(), children.end(), s) != children.end();
+}
+
+
+void Group::divide(int threashold = 1) {
+
+	if (threashold <= (int)children.size()) {
+		auto left = Group();
+		auto right = Group();
+
+		partitionChildren(left, right);
+		if (left.children.size() != 0) {
+			makeSubgroup(left);
+		}
+		if (right.children.size() != 0) {
+			makeSubgroup(right);
+		}
+	}
+
+	for (auto child : children) {
+		child->shapeDivide(threashold);
+	}
+}
+
+void Group::setMaterial(const Material& s) {
+	for (auto child : children) {
+		child->setMaterial(s);
+	}
+}
--- RayTracerChallenge/Groups.h
+++ RayTracerChallenge/Groups.h
@@ -9,120 +9,19 @@ public:
 	std::vector<Shape*> children;
 	BoundingBox m_bounds;
 
-	~Group() {
-		for (auto child : children) {
-			if (dynamic_cast<Group*>(child)) {
-				// cast only to avoid warning
-				delete (Group*)child;
-			}
-		}
-	}
+	~Group();
 
 	//TODO: passing a local group variable as child
 	// breaks everything yay
-	void addChild(Shape* s) {
-		children.emplace_back(s);
-		s->parent = this;
-	
-		// TODO: update bounds if needed
-		boundsOf(true);
-	}
-
-	inline Intersections intersect(const Ray& ray)override {
-
-		if (!m_bounds.intersect(ray)) {
-			return {};
-		}
-
-		Intersections i;
-
-		for (auto shape : children) {
-			// TODO: do this somehow else, overload intersect to take a vector?
-			auto v = shape->shapeIntersect(ray);
-			for (auto objectIntersect : v.intersections)
-				i.intersections.insert(objectIntersect);
-		}
-		return i;
-	}
-
-	Tuple objectNormal(const Tuple& objectPoint, const Intersection* hit = nullptr)override {
-		return Tuple::vector(0, 0, 0);
-	}
-
-	BoundingBox boundsOf(bool update = false)override {
-		// TODO: cache the box, don't update if not needed
-		if (update) {
-			auto box = BoundingBox();
-
-			for (auto shape : children) {
-				auto cbox = shape->parentSpaceBoundsOf();
-				box.mergeBox(cbox);
-			}
-
-			m_bounds = box;
-		}
-		return m_bounds;
-	}
-
+	void addChild(Shape* s);
 	// TODO: do this better
-	void inline partitionChildren(Group& leftG, Group& rightG) {
-		BoundingBox left, right;
+	void partitionChildren(Group& leftG, Group& rightG);
+	void makeSubgroup(Group& sub);
 
-		auto bounds = m_bounds;
-		bounds.splitBounds(&left, &right);
-
-		std::vector<Shape*> newChildren;
-
-		for (auto child : children) {
-			auto childBounds = child->parentSpaceBoundsOf();
-			if (left.contains(childBounds)) {
-				leftG.addChild(child);
-			}
-			else if (right.contains(childBounds)) {
-				rightG.addChild(child);
-			}
-			else
-				newChildren.emplace_back(child);
-		}
-		children = newChildren;
-	}
-
-	inline bool includes(const Shape* s)override {
-		return std::find(children.begin(), children.end(), s) != children.end();
-	}
-
-	inline void makeSubgroup(Group& sub) {
-		auto g = new Group();
-		for (auto s : sub.children) {
-			g->addChild(s);
-		}
-		addChild(g);
-	}
-
-	inline void divide(int threashold = 1)override {
-
-		if (threashold <= (int)children.size()) {
-			auto left = Group();
-			auto right = Group();
-
-			partitionChildren(left, right);
-			if (left.children.size() != 0) {
-				makeSubgroup(left);
-			}
-			if (right.children.size() != 0) {
-				makeSubgroup(right);
-			}
-		}
-
-		for (auto child : children) {
-			child->shapeDivide(threashold);
-		}
-
-	}
-
-	inline void setMaterial(const Material& s)override {
-		for (auto child : children) {
-			child->setMaterial(s);
-		}
-	}
+	Intersections intersect(const Ray& ray)const override;
+	Tuple objectNormal(const Tuple& objectPoint, const Intersection* hit = nullptr)const override;
+	BoundingBox boundsOf(bool update = false)override;
+	void divide(int threashold = 1)override;
+	bool includes(const Shape* s)const override;
+	void setMaterial(const Material& s)override;
 };
