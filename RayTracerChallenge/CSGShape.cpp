86f8caed593e19db9415635804c692ca41cffad0
rgrabar <renco.grabar@gmail.com>
2023-03-18 13:34:33 +0100

    CSG shape cpp

--- RayTracerChallenge/CSGShape.cpp
+++ RayTracerChallenge/CSGShape.cpp
@@ -1,1 +1,95 @@
 #include "CSGShape.h"
+
+CSGShape::CSGShape(std::string _operation, Shape* _left, Shape* _right) : operation(_operation), left(_left), right(_right) {
+	left->parent = this;
+	right->parent = this;
+}
+
+bool CSGShape::intersectionAllowed(std::string op, bool lhit, bool inl, bool inr)const {
+	if (op == "union") {
+		return (lhit && !inr) || (!lhit && !inl);
+	}
+	else if (op == "intersection") {
+		return (lhit && inr) || (!lhit && inl);
+	}
+	else if (op == "difference") {
+		return (lhit && !inr) || (!lhit && inl);
+	}
+
+	return false;
+}
+
+Intersections CSGShape::filterIntersections(const Intersections& xs)const {
+	auto inl = false;
+	auto inr = false;
+
+	auto result = Intersections();
+
+	for (auto intersection : xs.intersections) {
+
+		// TODO: page 235 implement Group and CSG serach
+		auto lhit = left->includes((Shape*)intersection->s);
+
+		if (intersectionAllowed(operation, lhit, inl, inr)) {
+			result.intersections.insert(intersection);
+		}
+		if (lhit) {
+			inl = !inl;
+		}
+		else {
+			inr = !inr;
+		}
+	}
+
+	return result;
+}
+
+Intersections CSGShape::intersect(const Ray& ray)const {
+	if (!m_bounds.intersect(ray)) {
+		return {};
+	}
+	auto leftxs = left->shapeIntersect(ray);
+	auto rightxs = right->shapeIntersect(ray);
+
+	auto xs = Intersections();
+
+	xs.intersections.insert(leftxs.intersections.begin(), leftxs.intersections.end());
+	xs.intersections.insert(rightxs.intersections.begin(), rightxs.intersections.end());
+
+	return filterIntersections(xs);
+}
+
+Tuple CSGShape::objectNormal(const Tuple& objectPoint, const Intersection* hit = nullptr)const {
+	// CSG returns normal of primitive shape, this shouldn't happen
+	return Tuple::vector(0, 0, 0);
+}
+
+BoundingBox CSGShape::boundsOf(bool update = false) {
+	// TODO: cache the box, don't update if not needed
+	// TODO: check if it works
+	auto box = BoundingBox();
+
+	auto rBox = right->parentSpaceBoundsOf();
+	auto lBox = left->parentSpaceBoundsOf();
+
+	box.mergeBox(rBox);
+	box.mergeBox(lBox);
+
+	m_bounds = box;
+
+	return m_bounds;
+}
+// TODO: cache bounds?
+void CSGShape::divide(int threashold = 1)const {
+	right->divide();
+	left->divide();
+}
+
+bool CSGShape::includes(const Shape* s)const {
+	return left->includes(s) || right->includes(s);
+}
+
+void CSGShape::setMaterial(const Material& s) {
+	right->material = s;
+	left->material = s;
+}
--- RayTracerChallenge/CSGShape.h
+++ RayTracerChallenge/CSGShape.h
@@ -9,99 +9,15 @@ public:
 	Shape* right;
 	BoundingBox m_bounds;
 
-	CSGShape(std::string _operation, Shape* _left, Shape* _right) : operation(_operation), left(_left), right(_right) {
-		left->parent = this;
-		right->parent = this;
-	}
+	CSGShape(std::string _operation, Shape* _left, Shape* _right);
+	bool intersectionAllowed(std::string op, bool lhit, bool inl, bool inr)const;
+	Intersections filterIntersections(const Intersections& xs)const;
 
-	Tuple objectNormal(const Tuple& objectPoint, const Intersection* hit = nullptr) {
-		// CSG returns normal of primitive shape, this shouldn't happen
-		return Tuple::vector(0, 0, 0);
-	}
-
+	Intersections intersect(const Ray& ray)const override;
+	Tuple objectNormal(const Tuple& objectPoint, const Intersection* hit = nullptr)const override;
 	// TODO: cache bounds?
-	inline void divide(int threashold = 1) {
-		right->divide();
-		left->divide();
-	}
-
-	inline bool intersectionAllowed(std::string op, bool lhit, bool inl, bool inr) {
-		if (op == "union") {
-			return (lhit && !inr) || (!lhit && !inl);
-		}
-		else if (op == "intersection") {
-			return (lhit && inr) || (!lhit && inl);
-		}
-		else if (op == "difference") {
-			return (lhit && !inr) || (!lhit && inl);
-		}
-
-		return false;
-	}
-
-	inline Intersections filterIntersections(const Intersections& xs) {
-		auto inl = false;
-		auto inr = false;
-
-		auto result = Intersections();
-
-		for (auto intersection : xs.intersections) {
-		
-			// TODO: page 235 implement Group and CSG serach
-			auto lhit = left->includes((Shape *)intersection->s);
-
-			if (intersectionAllowed(operation, lhit, inl, inr)) {
-				result.intersections.insert(intersection);
-			}
-			if (lhit) {
-				inl = !inl;
-			}
-			else {
-				inr = !inr;
-			}	
-		}
-
-		return result;
-	}
-
-	inline Intersections intersect(const Ray& ray) {
-		if (!m_bounds.intersect(ray)) {
-			return {};
-		}
-		auto leftxs = left->shapeIntersect(ray);
-		auto rightxs = right->shapeIntersect(ray);
-		
-		auto xs = Intersections();
-
-		xs.intersections.insert(leftxs.intersections.begin(), leftxs.intersections.end());
-		xs.intersections.insert(rightxs.intersections.begin(), rightxs.intersections.end());
-
-		return filterIntersections(xs);
-	}
-
-	inline bool includes(const Shape* s) {
-		return left->includes(s) || right->includes(s);
-	}
-
-	BoundingBox boundsOf(bool update = false) {
-		// TODO: cache the box, don't update if not needed
-		// TODO: check if it works
-		auto box = BoundingBox();
-
-		auto rBox = right->parentSpaceBoundsOf();
-		auto lBox = left->parentSpaceBoundsOf();
-
-		box.mergeBox(rBox);
-		box.mergeBox(lBox);
-
-		m_bounds = box;
-
-		return m_bounds;
-	}
-
-	inline void setMaterial(const Material& s) {
-		right->material = s;
-		left->material = s;
-	}
-
+	BoundingBox boundsOf(bool update = false) override;
+	void divide(int threashold = 1)const override;
+	bool includes(const Shape* s)const override;
+	void setMaterial(const Material& s)override;
 };
