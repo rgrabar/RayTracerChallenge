28f95186adbeec6d3e2211f5114bca0c99432630
rgrabar <renco.grabar@gmail.com>
2023-04-09 21:24:42 +0200

    build files

--- buildClang.sh
+++ buildClang.sh
@@ -1,1 +1,17 @@
-clang++ RayTracerChallenge/*.cpp -o RayTracer -O2 -std=gnu++17 -IRayTracerChallenge -pthread
+#clang++ RayTracerChallenge/*.cpp -o RayTracer -O2 -std=gnu++17 -IRayTracerChallenge -pthread -Wall -pedantic -Wextra
+#read -n 1 -s -r -p "Press any key to continue..."
+#!/bin/bash
+
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    make='make'
+    linkflags="-lGL -lm -lpthread -ldl -lrt -lX11"
+else # I'm not rich so I can't and categorically refuse to even attempt to test on a mac so this assumes the only other option is windows
+    make='mingw32-make'
+    linkflags="-lopengl32 -lgdi32 -lwinmm"
+fi
+
+pushd raylib/src
+$make PLATFORM=PLATFORM_DESKTOP
+popd
+
+clang++ RayTracerChallenge/*.cpp -o RayTracer -O2 -std=c++17 -IRayTracerChallenge -Iraylib/src -Lraylib/src -lraylib $linkflags
--- buildgcc.sh
+++ buildgcc.sh
@@ -1,1 +1,17 @@
-g++ RayTracerChallenge/*.cpp -o RayTracer -O2 -std=gnu++17 -IRayTracerChallenge -pthread
+#clang++ RayTracerChallenge/*.cpp -o RayTracer -O2 -std=gnu++17 -IRayTracerChallenge -pthread -Wall -pedantic -Wextra
+#read -n 1 -s -r -p "Press any key to continue..."
+#!/bin/bash
+
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    make='make'
+    linkflags="-lGL -lm -lpthread -ldl -lrt -lX11"
+else # I'm not rich so I can't and categorically refuse to even attempt to test on a mac so this assumes the only other option is windows
+    make='mingw32-make'
+    linkflags="-lopengl32 -lgdi32 -lwinmm -lstdc++"
+fi
+
+pushd raylib/src
+$make PLATFORM=PLATFORM_DESKTOP
+popd
+
+gcc RayTracerChallenge/*.cpp -o RayTracer -O2 -std=c++17 -IRayTracerChallenge -Iraylib/src -Lraylib/src -lraylib $linkflags
