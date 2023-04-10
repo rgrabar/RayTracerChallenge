627d1d7641fe2b68c4091037a3f8459fa5052ac0
rgrabar <renco.grabar@gmail.com>
2023-04-10 14:12:42 +0200

    Add link

--- buildgcc.sh
+++ buildgcc.sh
@@ -1,10 +1,11 @@
+#!/bin/bash
 #clang++ RayTracerChallenge/*.cpp -o RayTracer -O2 -std=gnu++17 -IRayTracerChallenge -pthread -Wall -pedantic -Wextra
 #read -n 1 -s -r -p "Press any key to continue..."
-#!/bin/bash
+
 
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
     make='make'
-    linkflags="-lGL -lm -lpthread -ldl -lrt -lX11"
+    linkflags="-lGL -lm -lpthread -ldl -lrt -lX11 -lstdc++"
 else # I'm not rich so I can't and categorically refuse to even attempt to test on a mac so this assumes the only other option is windows
     make='mingw32-make'
     linkflags="-lopengl32 -lgdi32 -lwinmm -lstdc++"
