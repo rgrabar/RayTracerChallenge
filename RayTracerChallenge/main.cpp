9db15017b72d99b4222fa53ef88be1f4242c6f95
rgrabar <renco.grabar@gmail.com>
2023-04-25 20:58:42 +0200

    help for focal blur

--- RayTracerChallenge/main.cpp
+++ RayTracerChallenge/main.cpp
@@ -44,6 +44,10 @@ int main(int argc, char* argv[]) {
 			std::cout << "\t--highlights\t\t 0/1, if enabled previews what will be antialiased when using the --edge option\n";
 			std::cout << "\t--threshold\t\t float [0, 1], if --edge is enabeld set the threshold for edge detection\n";
 			std::cout << "\t\t\t\t it checks for the difference in color beween the center of the pixel and it's edges\n";
+			std::cout << "\t--focal-length\t\t [double], the distance from the camera origin to the canvas\n";
+			std::cout << "\t\t\t\t by changing the focal length you can change which objects are in focus when using a non-zero aperture size\n";
+			std::cout << "\t--aperture-radius\t [double], camera aperture radius, the larger the value the more focal blur \n";
+			std::cout << "\t--apertureSamples\t [int], number of aprture samples to be jittered inside an aperture radius\n";
 			std::cout << "\t--path\t\t\t sets the path to the custom .obj file (can be used only with LoadOBJ scene)\n\n";
 
 			std::cout << "Other options: \n";
