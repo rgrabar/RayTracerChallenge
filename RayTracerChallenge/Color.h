16b6499d4d95225293910a16d6b382f531885483
rgrabar <renco.grabar@gmail.com>
2023-05-22 21:14:57 +0200

    grouped to make more sense

--- RayTracerChallenge/Color.h
+++ RayTracerChallenge/Color.h
@@ -6,15 +6,13 @@ struct Color {
 	Color(double r, double g, double b);
 	double r, g, b;
 	Color operator*(double scalar)const;
-	Color operator*(const Color& secondColor)const;
 	Color& operator*=(const double scalar);
-	Color& operator*=(const Color& secondColor);
-
 	Color operator/(double scalar)const;
 
+	Color operator*(const Color& secondColor)const;
+	Color& operator*=(const Color& secondColor);
 	Color operator+(const Color& secondColor)const;
 	Color& operator+=(const Color& secondColor);
-	
 	Color operator-(const Color& secondColor)const;
 	
 	bool operator==(const Color& secondColor)const;
